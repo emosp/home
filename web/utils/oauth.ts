e596a9a0fd02c99b788b5efd9c7f041514cfb236
puzzle <happypuzzle@126.com>
2025-09-26 22:57:31 +0800

    feat oauth telegram

--- web/utils/oauth.ts
+++ web/utils/oauth.ts
@@ -1,5 +1,5 @@
 import logo_linuxdo from '@/assets/logo/linuxdo.webp'
-import { Github, Google } from '@vicons/fa'
+import { Github, Google, Telegram } from '@vicons/fa'
 
 export const ToSignUrl = (name) => `/api/sign/to/${name}`
 
@@ -27,5 +27,11 @@ export const Oauths: Array<{
     logo_component: Google,
     label: 'Google',
     url: ToSignUrl('google'),
+  },
+  {
+    type: 'telegram',
+    logo_component: Telegram,
+    label: 'Telegram',
+    url: ToSignUrl('telegram'),
   },
 ]
