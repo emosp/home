07793607a2d3e86218b27eed7ee76292e5c3d46f
puzzle <happypuzzle@126.com>
2025-11-16 21:07:32 +0800

    fix link token exp

--- web/views/link.vue
+++ web/views/link.vue
@@ -7,22 +7,30 @@
         <n-button text @click="storeSign.showEntry()">请登录后访问</n-button>
       </template>
       <template #action v-if="storeSign.is_sign">
-        <n-button type="primary" tag="a" :href="link_to"> 同意 </n-button>
-        <n-button
-          style="margin-left: 10px"
-          type="error"
-          @click="
-            router.push({
-              name: ROUTE_NAME_INDEX,
-            })
-          ">
-          拒绝
-        </n-button>
+        <div class="flex mt-5 justify-center" v-if="loading">
+          <n-spin size="large">
+            <template #description> 验证中 </template>
+          </n-spin>
+        </div>
+        <div v-else>
+          <n-button type="primary" tag="a" :href="link_to"> 同意 </n-button>
+          <n-button
+            style="margin-left: 10px"
+            type="error"
+            @click="
+              router.push({
+                name: ROUTE_NAME_INDEX,
+              })
+            ">
+            拒绝
+          </n-button>
+        </div>
       </template>
     </n-card>
   </div>
 </template>
 <script setup lang="ts">
+  import instance from '@/utils/ky'
   import { ref } from 'vue'
   import { nMessage } from '@/utils/naive'
   import { useRouter, useRoute } from 'vue-router'
@@ -39,11 +47,30 @@
     link_to = ref(),
     link_uuid = route.params.uuid
 
-  if (!storeSign.is_sign) {
+  const loading = ref(true)
+
+  if (storeSign.is_sign) {
+    instance
+      .get('/api/sign/isSign', {
+        searchParams: {
+          link_uuid,
+        },
+      })
+      .then(async (res) => {
+        let { is_sign } = await res.json()
+        if (!is_sign) {
+          nMessage().error('请登录后再访问')
+          storeSign.tokenRemove()
+          storeSign.showEntry()
+        }
+      })
+      .finally(() => {
+        link_to.value = `${route.query.url}?username=${storeSign.user_username}&avatar=${storeSign.user_avatar}&token=${storeSign.user_token}`
+        loading.value = false
+      })
+  } else {
     nMessage().error('请登录后再访问')
     storeSign.showEntry()
-  } else {
-    link_to.value = `${route.query.url}?username=${storeSign.user_username}&avatar=${storeSign.user_avatar}&token=${storeSign.user_token}`
   }
 </script>
 <style scoped lang="stylus"></style>
